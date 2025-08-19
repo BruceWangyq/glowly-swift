7204e9fc8b267184deb5b44b72acbfe52a458c92
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2025-08-19 00:30:58 -0400

    Initial Commit

---
+++ glowly/glowlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  glowlyApp.swift
+//  glowly
+//
+//  Created by wang yuqiu on 2025-08-19.
+//
+
+import SwiftUI
+
+@main
+struct glowlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ glowlyTests/glowlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  glowlyTests.swift
+//  glowlyTests
+//
+//  Created by wang yuqiu on 2025-08-19.
+//
+
+import Testing
+@testable import glowly
+
+struct glowlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
